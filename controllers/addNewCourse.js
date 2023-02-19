15e433ee0680ec5b52140c222a4d65fa1d4dfc15
IdioticMarlin22 <jcrantfo@comcast.net>
2023-02-19 17:43:29 -0500

    Created addNewCourse controller modularized database.json files

---
+++ controllers/addNewCourse.js
@@ -0,0 +1,31 @@
+// Adds the newCourse object to the Courses.JSON file
+// Takes a newCourse Object as a parameter
+// newCourse object should fit Courses.JSON schema
+
+const fs = require('fs');
+
+module.exports = async function addNewCourse (newCourse) {
+  if (typeof newCourse !== 'object') {
+    console.log('newCourse is not of type "object"');
+    return;
+  }
+  await fs.readFile('./models/Courses.JSON', (err, data) => {
+    if (err) {
+      console.log(`Error: ${err}`);
+      return;
+    }
+    const dataObject = JSON.parse(data);
+    const allCourses = dataObject.Courses;
+  
+    allCourses.push(newCourse);
+    dataObject['Courses'] = allCourses;
+    
+    fs.writeFile('./models/Courses.JSON', JSON.stringify(dataObject), (err) => {
+      if (err) {
+        console.log(`Error: ${err}`);
+        return;
+      }
+      console.log('Courses.JSON updated');
+    })
+  })
+}
