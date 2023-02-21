ae39c86515f5281dc2326e04eada54e1bd8e3284
Anthony Aldrich <anthonyaldrich@Live.com>
2023-02-21 12:11:25 -0500

    added get JSON to routes added teacher, courseimg, color to Courses.JSON added avatar to Users.JSON added JSON intergration into Home.html Nav Menu created

--- router.js
+++ router.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
+const mime = require('mime');
 
 /*
   GET REQUESTS
 */
+// Get Course Information [USING THIS JUST TO IMPLEMENT INTO HTML, CAN BE REMOVED FOR CONTROLLER]
+router.get('/getCourses', (req, res) => {
+  const filePath = path.join(__dirname, 'models', 'Courses.JSON');
+  res.setHeader('Content-Type', mime.getType(filePath));
+  res.sendFile(filePath);
+});
 // Home
 router.get('/', (req, res) => {
   res.sendFile(`${__dirname}/pages/home.html`);
