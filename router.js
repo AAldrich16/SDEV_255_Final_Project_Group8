db521676e8cc09597ddaea1e217e1d07d44530e9
Anthony Aldrich <anthonyaldrich@Live.com>
2023-02-23 16:56:07 -0500

    Styled add page, also implmented EJS for course page that allows ID to be access in URL

--- router.js
+++ router.js
@@ -2,16 +2,21 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const mime = require('mime');
+const fs = require ('fs');
 
 /*
   GET REQUESTS
 */
-// Get Course Information [USING THIS JUST TO IMPLEMENT INTO HTML, CAN BE REMOVED FOR CONTROLLER]
-router.get('/getCourses', (req, res) => {
-  const filePath = path.join(__dirname, 'models', 'Courses.JSON');
-  res.setHeader('Content-Type', mime.getType(filePath));
-  res.sendFile(filePath);
+// Get Course Information 
+router.get('/getAllCourses', (req, res) => {
+  getAllCourses()
+  .then( data => {
+    data = JSON.parse(data);
+    console.log(data);
+    res.send(data);
+  });
 });
+
 // Home
 router.get('/', (req, res) => {
   res.sendFile(`${__dirname}/pages/home.html`);
@@ -19,6 +24,11 @@ router.get('/', (req, res) => {
 // Add
 router.get('/add', (req, res) => {
   res.sendFile(`${__dirname}/pages/add.html`);
+});
+// Course Page
+router.get('/course/:id', (req, res) => {
+  const id = req.params.id;
+  res.render(`${__dirname}/pages/course.ejs`, {'id': id});
 });
 // Courses
 router.get('/courses', (req, res) => {
