b37f0648cdc62ca5fe797fbb768601a4a432ae14
IdioticMarlin22 <jcrantfo@comcast.net>
2023-02-28 20:31:17 -0500

    Check if course exists before creating

--- routes/create.js
+++ routes/create.js
@@ -7,24 +7,28 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-
-  //We need to implement a check to see if course exists already
-
-  const course = new Course({
-    name: req.body.name,
-    subject: req.body.subject,
-    description: req.body.desc,
-    image: req.body.image,
-    color: req.body.color,
-    credits: req.body.credits
-  });
-  course.save()
-  .then(() => {
-    console.log('new course saved to database');
-    // redirect to new course page [We'd have to pull the ID unless we set the id]
-    res.redirect('/')
+  console.log(req.body);
+  // Check if document already exists
+  Course.exists({ name: req.body.name })
+  .then( data => {
+    // If document exists sent code 204 and exit function
+    if (data !== null) {
+      res.status(204).end()
+      return;
+    }
+    const course = new Course(req.body);
+    course.save()
+    .then(() => {
+      console.log('new course saved to database');
+      res.redirect('../')
+    })
+    .catch(err => console.log(err));
   })
-  .catch(err => console.log(err));
+  .catch(err => {
+    console.log(err)
+  });
+  
+  
 })
 
 module.exports = router;
