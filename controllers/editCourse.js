7c8bddb7ad37b64b363b8cbf68777a26eb358a2f
Anthony Aldrich <anthonyaldrich@Live.com>
2023-02-28 22:56:51 -0500

    edit course

--- controllers/editCourse.js
+++ controllers/editCourse.js
@@ -1,63 +1,47 @@
 const Course = require('../models/course');
 
 
-function createCourse(course) {
-  return  new Promise( (resolve, reject) => {
-    // Check if document already exists
-    Course.exists({ name: course.name })
-    .then( data => {
-      // If document exists, send code 204 and exit function
-      if (data !== null) {
-        res.status(204).end()
-        return;
-      }
-      else {
+function addCourse(course) {
+    return  new Promise( (resolve, reject) => {
         const newCourse = new Course(course);
         newCourse.save()
-        .then(() => {
-          resolve(newCourse);
-        })
-        .catch(err => {
-          reject(err.message);
-        })
-      }
+            .then(() => {
+                resolve('New course created');
+            })
+            .catch(err => {
+                reject(err);
+            });
     })
-    .catch(err => {
-      reject(err.message);
-    });
-  })
-};
-
+}
 
 function deleteCourse(courseID) {
-  return new Promise( (resolve, reject) => {
-    Course.findByIdAndDelete(courseID)
-    .then(() => {
-      resolve(`Course with id ${courseID} removed`);
+    return new Promise( (resolve, reject) => {
+        Course.findByIdAndDelete(courseID)
+            .then(() => {
+                resolve(`Course with id ${courseID} removed`);
+            })
+            .catch(err => {
+                reject(err);
+            });
     })
-    .catch(err => {
-      reject(err.message);
-    });
-  })
-};
-
+}
 
 // courseID is the course's _id key
 // updates is an object like { name: "SDEV 255" }
 function updateCourse(courseID, updates) {
-  return new Promise( (resolve, reject) => {
-    Course.findByIdAndUpdate(courseID, updates)
-    .then( () => {
-      resolve(`Updated document with id ${courseID}`)
-    })
-    .catch(err => {
-      reject(err.message);
-    })
-  });
-};
+    return new Promise( (resolve, reject) => {
+        Course.findByIdAndUpdate(courseID, updates)
+            .then( () => {
+                resolve(`Updated document with id ${courseID}`)
+            })
+            .catch(err => {
+                reject(err);
+            })
+    });
+}
 
 module.exports = {
-  createCourse,
-  deleteCourse,
-  updateCourse
+    addCourse,
+    deleteCourse,
+    updateCourse
 };
